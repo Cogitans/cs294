c5ac68a246ef89d9b757b2622197e0f63698246c
Aidan Clark <aidanbclark@berkeley.edu>
2016-10-21 21:34:20 -0700

    Initial RNN

--- code/LogRegSpeaker.py
+++ code/LogRegSpeaker.py
@@ -10,9 +10,9 @@ import numpy as np
 from keras.optimizers import SGD, RMSprop, Adagrad, Adam
 from keras.models import Sequential, Model, load_model
 from keras.layers import Merge, Input, merge
+from keras.callbacks import Callback
 from keras.layers.recurrent import GRU
 from keras.layers.core import Dense, Activation, Reshape, Flatten
-from scipy.sparse import vstack
 
 DIR = "../datasets/shakespeare/"
 
@@ -25,7 +25,7 @@ d = pickle.load(open(OUTPATH, "rb"))[0]
 LOAD = False
 do_PCA = False
 do_DOC = False
-RECUR = False
+RECUR = True
 
 if LOAD:
 	data = []
@@ -56,7 +56,8 @@ if LOAD:
 				print(i)
 			tostack.append(np.reshape(model.infer_vector(sent), (1, 300)))
 			i += 1
-		mat = np.vstack(tostack)
+		print(tostack[0])
+		mat = vstack(tostack)
 		vectors = vstack(tuple([mat[i, :] - mat[i+1, :] for i in xrange(1, mat.shape[0]-1)]))
 		print("THERE")
 
@@ -97,8 +98,8 @@ p_s = [i for i in xrange(len(labels)) if labels[i] == 1]
 n_s = [i for i in xrange(len(labels)) if labels[i] == 0]
 p_data, p_labels = vectors[p_s, :], [1 for i in p_s]
 n_data, n_labels = vectors[n_s, :], [0 for i in n_s]
-even_p_data, even_p_labels = p_data[0:30000, :], p_labels[0:30000]
-even_n_data, even_n_labels = n_data[0:30000, :], n_labels[0:30000]
+even_p_data, even_p_labels = p_data[5000:30000, :], p_labels[5000:30000]
+even_n_data, even_n_labels = n_data[5000:30000, :], n_labels[5000:30000]
 print(even_n_data.shape)
 print(even_p_data.shape)
 
@@ -109,7 +110,7 @@ pn_labels = even_n_labels + even_p_labels
 mask = np.random.permutation(len(pn_labels))
 
 pn_data = pn_data[mask, :]
-pn_labels = np.array(pn_labels)[mask, :]
+pn_labels = np.array(pn_labels)[mask]
 
 # pn_data = np.empty((even_n_data.shape[0] + even_p_data.shape[0], even_p_data.shape[1]))
 # pn_data[::2, :] = even_p_data
@@ -121,18 +122,47 @@ v_data, v_labels = vectors[TRAINING_NUM+1:], labels[TRAINING_NUM+1:]
 
 
 
-lr = RandomForestClassifier(n_estimators=100)
-print("Begun Training")
-lr.fit(pn_data, pn_labels)
-print("The val accuracy is: " + str(lr.score(v_data, v_labels)))
-print("The train accuracy is: " + str(lr.score(t_data, t_labels)))
-print("The positive accuracy is: " + str(lr.score(p_data, p_labels)))
-print("The negative accuracy is: " + str(lr.score(n_data, n_labels)))
+# lr = LogisticRegression()
+# print("Begun Training")
+# lr.fit(pn_data, pn_labels)
+# print("The val accuracy is: " + str(lr.score(v_data, v_labels)))
+# print("The train accuracy is: " + str(lr.score(t_data, t_labels)))
+# print("The positive accuracy is: " + str(lr.score(p_data, p_labels)))
+# print("The negative accuracy is: " + str(lr.score(n_data, n_labels)))
+
+data = mat[1:, :]
+print(data.shape)
+labels = np.array(labels)
+labels = labels.reshape((labels.shape[0], 1))
+BATCH_SIZE = 128
+NUM_EPOCH = 2
+MODEL_PATH = ("./keras.model")
 
 if RECUR:
-	MID_LAYER = 50
+	MID_LAYER = 5
 	network = Sequential()
-	network.add(GRU(MID_LAYER, input_dim=(mat.shape)))
-	network.add(GRU(1))
-	network.compile(optimizer="adam")
-	network.fit(mat, labels)
+	network.add(GRU(MID_LAYER, input_shape=mat.shape))
+	network.add(Dense(1))
+	network.compile(loss='mse', optimizer="adam")
+
+	def data_generator(data, labels):
+		i = 0
+		while True:
+			print(data[i*BATCH_SIZE:(i+1)*BATCH_SIZE, :])
+			yield (data[i*BATCH_SIZE:(i+1)*BATCH_SIZE, :].toarray(), labels[i*BATCH_SIZE:(i+1)*BATCH_SIZE])
+			i = i + 1 if (i+1)*BATCH_SIZE < data.shape[0]-1 else 0
+
+	# Keras doesn't come with a way to save a neural network every E epochs 
+	# (only every epoch, which here takes too long to be feasible).
+	# This is a simple implementation of a subclass of a Keras class, which
+	# lets us save our model to disk every E epochs (here E = 100)
+	class DelayedCheckpoint(Callback):
+		def on_epoch_end(self, batch, logs):
+			if (batch % 100) == 0 and batch != 0:
+				self.model.save(MODEL_PATH)
+
+	print("Training model.")
+
+	# This trains the network
+	network.fit_generator(data_generator(data, labels), BATCH_SIZE+1, callbacks=[DelayedCheckpoint()], max_q_size=2, verbose=2, nb_epoch=NUM_EPOCH)
+	network.save(MODEL_PATH)
